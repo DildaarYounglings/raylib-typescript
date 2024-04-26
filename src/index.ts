5185f0c8c2121aa038ab8fc9cae88de76f50d0e9
DildaarYounglings <dildaar.abrahams@younglings.africa>
2024-04-26 10:22:22 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import * as raylib from "raylib";
+const rl = raylib;
+
+const main = function(){
+    const darkGreen = rl.GetColor(0x0ff00);
+    const screenWidth = 800;
+    const screenHeigt = 600;
+    rl.InitWindow(screenWidth,screenHeigt,"raylib Tetris");
+    while(!rl.WindowShouldClose()){
+
+    }
+}
